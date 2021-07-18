0ca5aa3d9a68ddf01a39b4918600eed2d5ca5050
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-07-18 18:36:00 +0300

    cleaning the code:

--- src/app/quotes-main-section/quotes-main-section.component.ts
+++ src/app/quotes-main-section/quotes-main-section.component.ts
@@ -55,13 +55,6 @@ export class QuotesMainSectionComponent implements OnInit {
   }
 
 
-
-//   const input = [5, 150, 2, 8, 58, 4];
-
-// const result = input.sort((x, y) => y - x).slice(0, 2);
-
-// console.log(result);
-
   constructor() { }
 
   ngOnInit(): void {
